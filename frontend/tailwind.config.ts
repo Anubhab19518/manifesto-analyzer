f809a0d19ad1992098704551233dc93490c42317
Anubhab19518 <anubhab.19518@gmail.com>
2025-07-24 23:23:29 +0530

    Fixed Layout for deployment

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {}, // No font extensions needed in this simplified version
+    extend: {},
   },
   plugins: [],
 };
